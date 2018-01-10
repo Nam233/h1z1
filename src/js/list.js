577f6ab915956529d3065b764e1929fa8be48d9c
nam233 <532742670@qq.com>
2018-01-10 11:50:36 +0800

    ddd

--- src/js/list.js
+++ src/js/list.js
@@ -1,25 +1,31 @@
-// require(["config"], function(){
-// 	require(["jquery", "template", "load"], function($, template){
-// 		// 异步加载列表页面数据，使用模板引擎渲染
-// 		$.getJSON("/mock/list.json", function(data){
-// 			// 准备渲染数据
-// 			var renderData = {products : data.res_body.data};
-// 			// 渲染数据
-// 			var html = template("list_template", renderData);
-// 			$(".main").html(html);
-// 		});
-// 	});
-// });
 require(["config"], function(){
-	require(["load"], function(){
-		$(".main .list .btn").click(function(){
-			location.href="/html/cart.html";
+	require(["jquery", "template", "load"], function($, template){
+		// 异步加载列表页面数据，使用模板引擎渲染
+		$.getJSON("/mock/list.json", function(data){
+			// 准备渲染数据
+			var renderData = {products : data.res_body.data};
+			// 渲染数据
+			var html = template("list_template", renderData);
+			$("#product_list").html(html);
 		});
-		$(".main .list img").click(function(){
-			location.href = "/html/detail.html";
-		});
-		$(".main .list .title").click(function(){
-			location.href = "/html/detail.html";
-		});
+			$("#product_list").on('click','.btn',function(event) {
+				/* Act on the event */
+				window.location.href="/html/cart.html";
+			});
+			$("#product_list").on('click','img',function(event) {
+				/* Act on the event */
+				window.location.href="/html/detail.html";
+			});
+			$("#product_list").on('click','.title',function(event) {
+				/* Act on the event */
+				window.location.href="/html/detail.html";
+			});
+
+			// $(".main .list img").click(function(){
+			// 	location.href = "/html/detail.html";
+			// });
+			// $(".main .list .title").click(function(){
+			// 	location.href = "/html/detail.html";
+			// });
 	});
 });
--- src/js/register.js
+++ src/js/register.js
@@ -7,21 +7,41 @@ require(["config"], function(){
 		$("#h1z1_li").click(function(){
 			location.href="/html/list.html";
 		});
-		console.log($("#_btn"));
 		$("#_btn").click(function(event) {
 			/* Act on the event */
 			var username = $("#user_txt").val();
-			arr.forEach(function(curr){
-				if(curr.username == username){
-					allow = false;
-				}
-			});
-			if(allow){
-				alert("注册成功！！");
-				location.href = "/index.html";
+			var password = $("#psw_txt").val();
+			var pswagain = $("#psw_again").val();
+			if(username == ""||password == ""||pswagain == ""){
+				alert("请输入必填选项");
 			}else{
-				alert("用户名已存在！！");
-				allow = true;
+				arr.forEach(function(curr){
+					if(curr.username == username){
+						allow = false;
+					}
+				});
+				var reg = /^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/;
+				if (reg.test (password)){
+					if(password == pswagain){
+						if(allow){
+							alert("注册成功！！");
+							$.cookie('username', username, { expires: 7, path: '/' });
+							 location.href = "/index.html";
+						}else{
+							alert("用户名已存在！！");
+							$("#user_txt").val("");
+							$("#psw_txt").val("");
+							$("#psw_again").val("");
+							allow = true;
+						}
+					}else{
+						alert("两次输入密码请一致！");
+						$("#psw_txt").val("");
+						$("#psw_again").val("");
+					}
+				}else{
+					alert("密码需包含字母和数字！！");
+				}
 			}
 		});
 	});
